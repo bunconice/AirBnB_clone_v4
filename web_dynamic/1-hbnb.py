404659e8ec6c54ca44f99d778e1da27716d0010f
Bunmi Akinola <bunconice@gmail.com>
2024-02-11 23:31:27 +0100

    Script that starts a Flask Web App

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Script that starts a Flask Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -43,5 +43,4 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
